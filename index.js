1981cf98bb07dd568a6c46ab18c2c0530beeffe5
Andrii Sen <senanven@gmail.com>
2022-06-14 12:43:25 +0300

    Simple Users graphQL server, users are hardcoded in array

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const cors = require("cors");
+const schema = require("./schema");
+const users = [
+  {
+    id: 1,
+    username: "Andrii",
+    age: "27",
+  },
+  {
+    id: 2,
+    username: "Morbo",
+    age: "308",
+  },
+];
+
+const app = express();
+app.use(cors());
+
+const createUser = (input) => {
+  const id = Date.now();
+  return {
+    id,
+    ...input,
+  };
+};
+
+const root = {
+  getAllUsers: () => {
+    return users;
+  },
+  getUser: ({ id }) => {
+    return users.find((user) => user.id === id);
+  },
+  createUser: ({ input }) => {
+    const user = createUser(input);
+    users.push(user);
+    return user;
+  },
+};
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    graphiql: true,
+    schema,
+    rootValue: root,
+  })
+);
+
+app.listen(8080, () => console.log("Lintening on port 8080"));
